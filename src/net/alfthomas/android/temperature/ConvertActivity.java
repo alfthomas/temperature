6a4a5a60336a3f723cbd67fda5508eb683a4344c
alfthomas <alf.thomas.nilsen@gmail.com>
2011-12-18 00:48:19 +0100

    Implemented conversion between fahrenheit and celsius

--- src/net/alfthomas/android/temperature/ConvertActivity.java
+++ src/net/alfthomas/android/temperature/ConvertActivity.java
@@ -2,12 +2,57 @@ package net.alfthomas.android.temperature;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class ConvertActivity extends Activity {
+    
+    private EditText text;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        text = (EditText) findViewById(R.id.editText1);
     }
+    
+    public void myClickHandler(View view) {
+        switch(view.getId()) {
+            case R.id.button1:
+                RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
+                RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
+                if (text.getText().length() == 0) {
+                    Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                
+                float inputValue = Float.parseFloat(text.getText().toString());
+                if (celsiusButton.isChecked()) {
+                    text.setText(String.valueOf(convertFahrenheitToCelsius(inputValue)));
+                    celsiusButton.setChecked(false);
+                    fahrenheitButton.setChecked(true);
+                }
+                else {
+                    text.setText(String.valueOf(convertCelsiusToFahrenheit(inputValue)));
+                    fahrenheitButton.setChecked(false);
+                    celsiusButton.setChecked(true);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+
+    private float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+    
+    
 }
